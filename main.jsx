7c7ea03596ecb9acd7a5edf844f2147393f7c85c
manuel1601-cloud <hmanueladrian52@gmail.com>
2025-04-07 10:03:26 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GeneradorPrecios from "./GeneradorPrecios.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GeneradorPrecios />);
