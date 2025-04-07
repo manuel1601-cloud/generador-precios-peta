56e5b0d5d68eb73e14b262339e11efad8baed2f0
manuel1601-cloud <hmanueladrian52@gmail.com>
2025-04-07 11:47:49 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GeneradorPrecios from "./GeneradorPrecios.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<GeneradorPrecios />);
